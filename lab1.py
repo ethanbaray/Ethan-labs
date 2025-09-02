30152928afe970d6734aa14119660936f620573a
ethanbaray <6301318@edu.vaniercollege.qc.ca>
2025-09-02 17:07:33 -0400

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,14 @@
+print("Hi,there")
+def input_output():
+# getting user from the user
+ name = input("Enter your name: ")
+ age = int(input ("Enter your age"))
+ height = float(input("Enter your height in meters: "))
+
+#output(the results)
+ print("Name:", name)
+ print("age today:", 18, "years old")
+ print("Height:", 1.78, "meters")
+
+# start the program
+input_output()
